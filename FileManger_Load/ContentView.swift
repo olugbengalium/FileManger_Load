239983b32569c5b56b0192e38c5e517ab038c8f6
Akinbande Gbenga <olugbengalium@gmail.com>
2023-03-26 22:53:21 +0100

    Full Patch

--- FileManger_Load/ContentView.swift
+++ FileManger_Load/ContentView.swift
@@ -6,16 +6,170 @@
 //
 
 import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+class LocalFileManager {
+    static let instance = LocalFileManager()
+    let folderName = "My_App_Images"
+    init(){
+        createFolderIfNeeded()
+    }
+    func createFolderIfNeeded(){
+        guard
+            let path = FileManager
+                .default
+                .urls(for: .cachesDirectory, in: .userDomainMask)
+                .first?
+                .appendingPathComponent(folderName)
+                .path else{
+            return
         }
-        .padding()
+        if !FileManager.default.fileExists(atPath: path){
+            do{
+                try FileManager.default.createDirectory(atPath: path, withIntermediateDirectories: true, attributes: nil)
+                print("Success creating folder")
+            } catch let error {
+                print("Error creating folder \(error)")
+            }
+        }
+    }
+    func deleteFolder(){
+        guard
+            let path = FileManager
+                .default
+                .urls(for: .cachesDirectory, in: .userDomainMask)
+                .first?
+                .appendingPathComponent(folderName)
+                .path else{
+            return
+        }
+        do{
+            try FileManager.default.removeItem(atPath: path)
+            print("folder successfully deleted")
+        }catch let error{
+            print("error deleting folder \(error)")
+        }
+    }
+    
+    
+    func saveImage(image: UIImage, name: String) -> String{
+        guard let data = image.jpegData(compressionQuality: 1.0),
+            let path = getImagePath(name: name)
+        else{
+            return "Error getting data"
+        }
+        do{
+            try data.write(to: path)
+            print(path)
+            return "Success saving!"
+        }
+        catch let error{
+            print("error saving \(error)")
+            return "error saving!"
+        }
+    }
+    func getImage(name: String) -> UIImage? {
+        guard let path = getImagePath(name: name)?.path,
+                FileManager.default.fileExists(atPath: path)
+        else{
+            print ("error getting path")
+            return nil
+        }
+        return UIImage(contentsOfFile: path)
+    }
+    func getImagePath(name: String) -> URL?{
+        guard let path = FileManager
+            .default
+            .urls(for: .cachesDirectory, in: .userDomainMask)
+            .first?
+            .appendingPathComponent(folderName)
+            .appendingPathComponent("\(name).jpg")
+        else { print("error getting path")
+            return nil}
+        return path
+    }
+    func deleteImage(name: String) -> String{
+        guard let path = getImagePath(name: name),
+              FileManager.default.fileExists(atPath: path.path)
+        else{
+            return "error getting path"
+            
+        }
+        do {
+            try FileManager.default.removeItem(at: path)
+            return "File successfully deleted"
+        } catch let error{
+            return "file not successfully deleted. \(error)"
+        }
+    }
+}
+
+class FileManagerViewModel: ObservableObject {
+    @Published var image: UIImage? = nil
+    let imageName:String = "flow"
+    let manager = LocalFileManager.instance
+    @Published var infoMessage: String = ""
+    init(){
+        getImageFromAssetsFolder()
+//        getImageFromFileManager()
+        
+    }
+    func getImageFromFileManager(){
+        image = manager.getImage(name: imageName)
+    }
+    
+    func getImageFromAssetsFolder(){
+        image = UIImage(named: imageName)
+    }
+    func saveImage(){
+        guard let image = image else {return}
+        infoMessage = manager.saveImage(image: image, name: imageName)
+    }
+    func deleteImage(){
+        infoMessage = manager.deleteImage(name: imageName)
+        manager.deleteFolder()
+    }
+}
+struct ContentView: View {
+    @StateObject var vm = FileManagerViewModel()
+    var body: some View {
+        NavigationView {
+            VStack {
+                if let image = vm.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 200, height: 200)
+                        .clipped()
+                        .cornerRadius(30)
+                }
+                HStack{
+                    Button(action: {vm.saveImage()}, label:{
+                        Text("Save to FM")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .padding()
+                            .padding(.horizontal)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    })
+                    Button(action: {vm.deleteImage()}, label:{
+                        Text("Delete from FM")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .padding()
+                            .padding(.horizontal)
+                            .background(Color.red)
+                            .cornerRadius(10)
+                    })
+                }
+                Text(vm.infoMessage)
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.purple)
+             Spacer()
+            }
+            .navigationTitle("File Manager")
+            .padding(40)
+        }
     }
 }
 
@@ -24,3 +178,23 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+////        let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+//        let directory = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first
+//        let path = directory?.appendingPathComponent("\(name).jpg")
+//        print(directory)
+//        print(path)
+//        let directory3 = FileManager.default.temporaryDirectory
+//        print("file 1 \(directory) ")
+//        print("file 1 \(directory2) ")
+//        print("file 1 \(directory3) ")
+
+
+//guard let path = FileManager
+//    .default
+//    .urls(for: .cachesDirectory, in: .userDomainMask)
+//    .first?
+//    .appendingPathComponent("\(name).jpg")
+//else { print("error getting path")
+//    return}
