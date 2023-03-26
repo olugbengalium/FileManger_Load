a11af9997ea140e72fe55ebb6404d6c392379dc3
Akinbande Gbenga <olugbengalium@gmail.com>
2023-03-26 16:41:02 +0100

    Initial Commit

---
+++ FileManger_Load/FileManger_LoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileManger_LoadApp.swift
+//  FileManger_Load
+//
+//  Created by Gbenga Abayomi on 26/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FileManger_LoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
